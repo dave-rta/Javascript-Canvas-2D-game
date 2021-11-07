33900156f85fb5787177e720ea96c3f47a6662f3
David Plattner <daplit00@hs-esslingen.de>
2021-11-07 19:20:52 +0100

    changed from tap to move and added bullets

--- app.js
+++ app.js
@@ -6,22 +6,37 @@ canvas.height = 500;
 let score = 0;
 let timeFrame = 0;
 
-const finger = {
+const move = {
     x: canvas.width / 2,
     y: canvas.height / 2,
     tap: false
 }
 
+const bullets = [];
+
 //Top left 0:0
 let canvasPosition = canvas.getBoundingClientRect();
 //touch events
 canvas.addEventListener("touchstart", function (event) {
-    finger.tap = true;
-    finger.x = event.touches[0].clientX - canvasPosition.left;
-    finger.y = event.touches[0].clientY - canvasPosition.top;
+    event.preventDefault();
+    // canvas height offset????
+    const angle = Math.atan2((event.touches[0].clientY - 290) - move.y , event.touches[0].clientX - move.x);
+    console.log(angle);
+    const speed = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    bullets.push(new Bullet({x: speed.x * 5, y: speed.y * 5}));
 }, false);
+canvas.addEventListener('touchmove', function (event) {
+    event.preventDefault();
+    move.tap = true;
+    move.x = event.touches[0].clientX - canvasPosition.left;
+    move.y = event.touches[0].clientY - canvasPosition.top;
+}, false)
 canvas.addEventListener('touchend', function (event) {
-    finger.tap = false;
+    event.preventDefault();
+    move.tap = false;
 })
 
 //Player Images
@@ -42,24 +57,24 @@ class Player {
     }
 
     update() {
-        const dx = this.x - finger.x;
-        const dy = this.y - finger.y;
+        const dx = this.x - move.x;
+        const dy = this.y - move.y;
         // angle to face tap direction
         this.angle = Math.atan2(dy, dx);
-        
-        if (finger.x != this.x) {
+
+        if (move.x != this.x) {
             this.x -= dx / 30;
         }
-        if (finger.y != this.y) {
+        if (move.y != this.y) {
             this.y -= dy / 30;
         }
     }
     draw() {
-        if (finger.tap) {
+        if (move.tap) {
             context.lineWidth = 0.2;
             context.beginPath();
             context.moveTo(this.x, this.y);
-            context.lineTo(finger.x, finger.y);
+            context.lineTo(move.x, move.y);
             context.stroke();
         }
         //"Hit-box"
@@ -70,7 +85,7 @@ class Player {
         context.save();
         context.translate(this.x, this.y);
         context.rotate(this.angle);
-        if (this.x > finger.x) {
+        if (this.x > move.x) {
             //check if correct (img, next 4 area of pic, last 4 dest of image)
             context.drawImage(playerLeft, 0 * this.imgWidth, 0 * this.imgWidth, this.imgHeight, this.imgWidth, 0 - 70, 0 - 50, this.imgWidth / 4, this.imgHeight / 3);
         } else {
@@ -78,7 +93,6 @@ class Player {
             context.drawImage(playerRight, 0 * this.imgWidth, 0 * this.imgWidth, this.imgHeight, this.imgWidth, 0 - 75, 0 - 50, this.imgWidth / 4, this.imgHeight / 3);
         }
         context.restore();
-
     }
 }
 
@@ -173,11 +187,33 @@ function handleEnemy() {
         // take obj out of array
         if (i.y < 0 - i.radius * 2) {
             setTimeout(() => {
-                enemyArray.splice(y, 1);
+                enemyArray.splice(i, 1);
             })
         }
     })
 }
+
+// bullets
+class Bullet {
+    constructor(speed) {
+        this.x = move.x;
+        this.y = move.y;
+        this.radius = 10;
+        this.speed = speed;
+    }
+    update() {
+        this.x += this.speed.x;
+        this.y += this.speed.y;
+    }
+    draw() {
+        context.fillStyle = 'red';
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fill()
+        context.closePath();
+    }
+}
+
 
 function animate() {
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -185,6 +221,10 @@ function animate() {
     handleEnemy();
     player.update();
     player.draw();
+    bullets.forEach((bullet) => {
+        bullet.update();
+        bullet.draw();
+    })
     context.fillStyle = 'black';
     context.fillText('Score: ' + score, 5, 15);
     timeFrame++;
